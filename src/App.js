2d386b8659e9945bb0c6a445f61d909d36f14d49
Shane Johnson <shane.johnson@gamesys.co.uk>
2017-11-29 14:50:05 +0000

    removing react holding page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">Your mum!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="app-intro">
+          There will be more to come soon...
         </p>
       </div>
     );
